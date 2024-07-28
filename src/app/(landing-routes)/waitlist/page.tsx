0c2e5aa2ef2d759872127641a71e5d27a5c68a88
Bwesun <maturinnocent@gmail.com>
2024-07-28 16:03:03 +0100

    Fits and addings of card components.

--- src/app/(landing-routes)/waitlist/page.tsx
+++ src/app/(landing-routes)/waitlist/page.tsx
@@ -1,5 +1,15 @@
+import WaitlistCard from "~/components/common/WaitListForm/WaitlistCard";
+
 const WaitlistPage = () => {
-  return <div>WaitlistPage</div>;
+  return (
+    <WaitlistCard
+      cardTitle={"Easy Customization"}
+      cardDescription={
+        "Tailor your experience to your specific needs and preferences for maximum results"
+      }
+      cardIcon={"~/public/Waitlist/vector-icon.svg"}
+    />
+  );
 };
 
 export default WaitlistPage;
